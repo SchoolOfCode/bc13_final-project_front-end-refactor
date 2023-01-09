c5a7a7002a1509034542df5d30d4de41d3fb9978
Cristhyan <cristhyancalderon@hotmail.co.uk>
2023-01-09 16:05:18 +0000

    done the body of the home page plus styling

--- petup/pages/_app.js
+++ petup/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../component/header/header.css'
 import '../component/button/button.css'
+import '../styles/index.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- petup/pages/index.js
+++ petup/pages/index.js
@@ -1,11 +1,20 @@
-import Link from 'next/link';
-import Header from '../component/header/header';
+import Link from "next/link";
+import Header from "../component/header/header";
+import Button from "../component/button/button";
 
 export default function Home() {
   return (
     <>
       <Header />
-      
+      <div className="main-div">
+        <div className="searchBox-div">
+          <input className="input-field" type="text" />
+          <Button className="search-button" text="Search"></Button>
+        </div>
+        <div className="tagline-div">
+          <h1>Find pet sitters</h1>
+        </div>
+      </div>
     </>
   );
 }
