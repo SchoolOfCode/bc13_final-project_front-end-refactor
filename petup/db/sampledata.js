e0eafa51f1c45c28afaf22dbe4a5964a438f43db
aliafarhana88 <alia.abdulghaffar@gmail.com>
2023-01-10 16:29:38 +0000

    updated db with image and relevant data

--- petup/db/sampledata.js
+++ petup/db/sampledata.js
@@ -1,5 +1,89 @@
 //this is sample dummy data for first sprint
+//this data is more refined and catered for our app compared to dummydb.js
 
 export const user = [
-    
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "nickname": "Leanne",
+        "imageURL" : "https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?w=2000",
+        "tagline": "I am a pet carer",
+        "address": {
+          "street": "Kulas Light",
+          "city": "London",
+          "postcode": "WN5 9FT",
+        },
+        "phone": "1-770-736-8031 x56442",
+        "rate" : "35"
+      },
+      {
+        "id": 2,
+        "name": "Leanne Graham 2",
+        "nickname": "Leanne 2",
+        "imageURL" : "https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?w=2000",
+        "tagline": "I am a pet carer",
+        "address": {
+          "street": "Kulas Light",
+          "city": "London",
+          "postcode": "WN5 9FT",
+        },
+        "phone": "1-770-736-8031 x56442",
+        "rate" : "35"
+      },
+      {
+        "id": 3,
+        "name": "Leanne Graham",
+        "nickname": "Leanne 3",
+        "imageURL" : "https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?w=2000",
+        "tagline": "I am a pet carer",
+        "address": {
+          "street": "Kulas Light",
+          "city": "London",
+          "postcode": "WN5 9FT",
+        },
+        "phone": "1-770-736-8031 x56442",
+        "rate" : "35"
+      },
+      {
+        "id": 4,
+        "name": "Leanne Graham",
+        "nickname": "Leanne 4",
+        "imageURL" : "https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?w=2000",
+        "tagline": "I am a pet carer",
+        "address": {
+          "street": "Kulas Light",
+          "city": "London",
+          "postcode": "WN5 9FT",
+        },
+        "phone": "1-770-736-8031 x56442",
+        "rate" : "35"
+      },
+      {
+        "id": 5,
+        "name": "Leanne Graham",
+        "nickname": "Leanne 5",
+        "imageURL" : "https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?w=2000",
+        "tagline": "I am a pet carer",
+        "address": {
+          "street": "Kulas Light",
+          "city": "London",
+          "postcode": "WN5 9FT",
+        },
+        "phone": "1-770-736-8031 x56442",
+        "rate" : "35"
+      },
+      {
+        "id": 6,
+        "name": "Leanne Graham",
+        "nickname": "Leanne 6",
+        "imageURL" : "https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?w=2000",
+        "tagline": "I am a pet carer",
+        "address": {
+          "street": "Kulas Light",
+          "city": "London",
+          "postcode": "WN5 9FT",
+        },
+        "phone": "1-770-736-8031 x56442",
+        "rate" : "35"
+      },
 ]
