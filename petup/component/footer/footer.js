e0b99accb6ba20e47b709680c83586cebc4b54e6
francesco44Hello <francescolongo44@gmail.com>
2023-01-10 15:49:39 +0000

    added footer, almost finished CSS of landing page

---
+++ petup/component/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <h2>Contact us</h2>
+            <h2>PetApp</h2>
+            <h2>Copyright @ SoC</h2>
+        </footer>
+    )
+}
+
+export default Footer
