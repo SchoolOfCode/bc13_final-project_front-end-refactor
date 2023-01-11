545a8ef93bf26eac937aeced61b26aadf713c5cf
francesco44Hello <francescolongo44@gmail.com>
2023-01-11 10:37:29 +0000

    added contact page,link to home on logo and styling

---
+++ petup/pages/contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+import Header from '../component/header/header'
+const Contact = () => {
+    return (
+        <>
+           <Head>
+            <title>
+                Contact
+            </title>
+           </Head> 
+           <Header />
+        </>
+    )
+}
+
+export default Contact
