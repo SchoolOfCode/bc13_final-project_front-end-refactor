aae48a393dec4b3af7b88c34ee65118688b59dc6
francesco44Hello <francescolongo44@gmail.com>
2023-01-20 13:56:44 +0000

    modified colors, added become a sitter page and modifid style navbar search page

--- petup/pages/search.js
+++ petup/pages/search.js
@@ -66,15 +66,20 @@ const Search = ({sitterData}) => {
 
       <Header />
       <div className="search-filter-navbar">
-        <input
+      <div className="navbar-container-search">
+
+      <div className="navbar-input-fieald">
+      <input
           name="input"
           className="search-input-field"
           placeholder="Search"
           type="text"
         />
+      </div>
+        
         
         <div className="search-pet-service">
-          <select name="service" id="pets" className="toggle-box-service">
+          <select name="service" id="pets" className="search-toggle-box-service">
             <option value="Pet hosting">Pet Hosting</option>
             <option value="home sitting">Home Sitting</option>
             <option value="home sitting">Dog Walking</option>
@@ -87,11 +92,17 @@ const Search = ({sitterData}) => {
             <option value="Other">Other</option>
           </select>
         </div>
+        <div className="navbar-button-div">
         <Button className="sign-up" text="Update"></Button>
+        </div>
+       
+        </div>
       </div>
-
+      
       <div className="search-page-main-div">
+      <div className="white-main-div">
 
+      
       <div className="search-result">
 
       {/* <p>this is user input for city: ${city}</p> */}
@@ -115,6 +126,7 @@ const Search = ({sitterData}) => {
           );
         })}</div>
         <div className="map-div"><Map /></div>
+        </div>
         
       </div>
       <Footer />
