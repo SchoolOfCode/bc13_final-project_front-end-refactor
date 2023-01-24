eed887dd8a7a29279bcbe4a6fc1735463f0db43d
Cristhyan <cristhyancalderon@hotmail.co.uk>
2023-01-24 14:34:53 +0000

    fixed typo in _appjs

--- petup/pages/_app.js
+++ petup/pages/_app.js
@@ -22,7 +22,7 @@ import '../styles/sitter.page.css'
 import { SessionProvider } from "next-auth/react"
 
 export default function App({
-  Component, session,
+  Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
